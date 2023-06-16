f991912ea26036ed911f60ae3c64739b94677171
RanggaHijriawaHaryadi <rangghrydi04@gmail.com>
2023-06-16 15:05:52 +0700

    membuat fungsi empty retrun bakal kembali ke null

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -35,6 +35,9 @@ public:
 		cout << "\nThe popped element is :" << stack_array[top] << endl; //step 2
 		top--;// step 3 decrement
 	}
-
+	// method for check if data is mepty
+	bool empty() {
+		return(top == -1);
+	}
 	
 };
